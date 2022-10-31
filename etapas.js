71fa12fbcc6a38d7f19dd7383e33480773cb68d6
Estevão <estevaobmalima@gmail.com>
2022-10-31 09:14:23 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 11111,
+                nome: '...',
+                partido: '...',
+                fotos: [
+                    {url: '', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 22222,
+                name: '...',
+                partido: '...',
+                fotos: [
+                    {url:'', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 77,
+                nome: 'Gru',
+                partido: 'MMF',
+                vice: 'Kevin',
+                fotos:[
+                    {url:'Candidato1.png', legenda: 'Prefeito'},
+                    {url:'Candidato1_1.png', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: 22,
+                nome: '...',
+                partido: '...',
+                vice: '...',
+                fotos:[
+                    {url:'', legenda: 'Prefeito'},
+                    {url:'', legenda: 'Vice-Prefeito', small:true}
+                ]
+            }
+        ]
+    }
+]
